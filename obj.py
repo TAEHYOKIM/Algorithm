69ed0637489a5cb471cc97792e75b2af534e8ce0
anjanghyeon <ajh910@gmail.com>
2018-03-14 16:24:57 +0900

    파일하나 추가해봤어.
    ㅋㅋ

---
+++ obj.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 16:24:10 2018
+
+@author: janghyeonan
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 12:15:15 2018
+
+@author: janghyeonan
+"""
+
+#pandas의 시작
+from pandas import Series,DataFrame
+import pandas as pd
+import numpy as np
+
+obj = Series([1,2,3,None,5])
+obj
+obj.isnull()
+
+print('kim')
+print('dd')
