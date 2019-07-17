1bb86841039599f37d3a13fbb1bd058e1c5c50f9
INGYEOMENTAL <37355339+TAEHYOKIM@users.noreply.github.com>
2019-07-17 13:51:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,5 @@ from pandas import Series, DataFrame
 s = Series([1,2,3])
 s
 print("hello jang 형")
-<<<<<<< HEAD
 print("hello 태효짱짱")
-=======
 print("hello 니니")
->>>>>>> 5d78979f09d6b410e5e1dc9a1b003af403d1e1fc
